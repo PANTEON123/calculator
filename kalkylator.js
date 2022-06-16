8933aa5c76d571b8ff4fbe77b711c92a516d5860
PANTEON <ebrikov@bk.ru>
2022-06-16 21:13:06 +0300

    finished calculator

---
+++ kalkylator.js
@@ -0,0 +1,55 @@
+let priv = "Привет";
+
+let isk;
+let i;
+
+function schet(number) {
+
+    input1.value += number;
+
+};
+eight.onclick = eight.addEventListener('click', schet(8));
+//pesen.onclick = pesen.addEventListener('click', hebdler17);
+let rut;
+
+function perenos() {
+    isk = eval(input1.value);
+    input1.value = isk;
+    //alert(typeof(isk));
+    // Number(isk);
+    //input2.value = isk;
+    //alert(isk);
+    //eval(isk);
+    // alert(isk);
+
+};
+
+function summsum() {
+    let obl = eval(+isk + +input1.value);
+    input1.value = obl;
+};
+
+function miysmin() {
+    let obl = eval(+input1.value - +isk);
+    input1.value = obl;
+};
+
+function remu() {
+    pyst = input1.value = "";
+    input1.value = pyst;
+};
+
+function koren() {
+
+    let ker = eval(input1.value ** (1 / 2));
+    input1.value = ker;
+};
+
+function Mce() {
+    isk = "0";
+    input1.value = isk;
+};
+
+function Mre() {
+    input1.value = isk;
+}
